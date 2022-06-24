de43fb6298d3f76d6946aba6e85737878eff3b30
MarS-27 <marchuk1992@gmail.com>
2022-06-24 18:33:58 +0300

    add some changes

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 
-arrA = [1, 2, 8, 'hello', 4, 5];
-arrB = [1, 2, 3, true, 4, undefined, 'abc', 13, 6];
+let arrA = [1, 2, 8, 'hello', 4, 5];
+let arrB = [1, 2, 3, true, 4, undefined, 'abc', 13, 6];
 
 function checkNumSum(a, b) {
 
     function getSum(arr) {
-        arrSum = 0;
-        for(i = 0; i < arr.length; i++) {
+
+        let arrSum = 0;
+
+        for(let i = 0; i < arr.length; i++) {
             if (typeof arr[i] === 'number') {
                 arrSum += arr[i];
             } 
         }
+        
         return arrSum;
     }
 
-    sumA = getSum(arrA);
-    sumB = getSum(arrB);
+    let sumA = getSum(arrA);
+    let sumB = getSum(arrB);
 
     if(sumA > sumB) {
         return arrA;
